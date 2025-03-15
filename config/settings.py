1219b389f16f4e0331d1f52935fc8d579b072fd8
elbek2327 <zubaydullayev1609@gmail.com>
2025-03-16 04:20:09 +0500

    errors updated

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -27,10 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -115,17 +111,39 @@ USE_I18N = True
 USE_TZ = True
 
 import os
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'shop/static')
 
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'shop/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'shop/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Project Falcon", #title
+    "site_header": "Admin", #header
+    "site_brand":"Admin menu",
+    "copyright":"Everything is under control",#copyright
+    "topmenu_links" : [
+        {
+            "app":"shop", #top dagi category sifat narsa
+        }
+    ],
+    "show_sidebar": True,
+    "show_search": True,
+    "navigation_expanded": True,
+    # "language_chooser":True
+    "user_avatar": None,
+    "site_logo":None, #logo uchun hozircha yoq logo
+    "site_icon":"shop/images/logo.png", #site iconim
+    "welcome_sign":"Welcome to admin menu",
+    "search_model": ["auth.User"],
+    "show_ui_builder": True,
+}
--- shop/admin.py
+++ shop/admin.py
@@ -5,22 +5,19 @@ from shop.models import Product,Category, Images
 
 admin.site.unregister(Group)
 
-admin.site.register(Category)
 
 
 
-# class ProductResource(resources.ModelResource):
-#     class Meta:
-#         model = Product
-#
-#
-# @admin.register(Product)
-# class ProductModelAdmin( admin.ModelAdmin):
-#     resource_class = ProductResource
-#     list_display = ['id','name','price','image','category']
-#     search_fields = ['name','description']
-#     list_filter = [ 'category']
-#
 
-admin.site.register(Product)
-admin.site.register(Images)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'price', 'quantity' )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'image' )
+
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ('id','product', 'image' )
