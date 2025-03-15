b09d72e6d9a89c025c1f94827128baf04b8a0d34
elbek2327 <zubaydullayev1609@gmail.com>
2025-03-16 03:48:40 +0500

    product list and product details modified

--- shop/urls.py
+++ shop/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product/details/', views.product_details, name='product_details'),
-    path('product/list/', views.product_list, name='product_list'),
+    path('product/details/<int:product_id>', views.product_details, name='product_details'),
+    path('product/details/html/', views.product_details_html, name='product_details_html'),
+    path('products-of-category/<int:category_id>/', views.product_list, name='product_list'),
+    path('product/list/html/', views.product_list_html, name='product_list_html'),
     path('products-of-category/<int:category_id>/', views.index, name='product_of_category'),
     path('product/images/<int:product_id>/', views.product_images, name='product_images'),
 
--- shop/views.py
+++ shop/views.py
@@ -3,41 +3,57 @@ from shop.models import Category, Product, Images
 # Create your views here.
 
 def index(request):
-    categories = Category.objects.all()  # Fetch all categories from the database
+    categories = Category.objects.all()
+    # Fetch all categories from the database
+    products = Product.objects.all()
     context = {
         'categories': categories,
+        'products': products,
     }
     return render(request, 'shop/index.html', context)
 
 
 
 
+def product_details_html(request):
+    return render(request, 'shop/product-details.html')
+
+
+
 
 def product_details(request, product_id:int | None = None):
-    product = Product.objects.get(id=product_id)
-
+    product = get_object_or_404(Product, id=product_id)
     context = {
         'product': product,
     }
-
     return  render(request, 'shop/product-details.html', context)
 
+
+def product_list_html(request):
+    return render(request, 'shop/product-list.html')
 def product_list(request, category_id :int | None = None):
-    categories = Category.objects.all()
+    category = get_object_or_404(Category, id=category_id)
+    products = Product.objects.filter(category_id=category_id)
+
+    context = {
+        'products': products,
+        'category': category,
+    }
+    return render(request, 'shop/product-list.html', context)
+
+
+def product_images(request, product_id:int | None = None, category_id :int | None = None):
+    product = get_object_or_404(Product, id=product_id)
+    images = Images.objects.filter(product=product)
 
     if category_id:
         products = Product.objects.filter(category_id=category_id)
-    else:
+    else :
         products = Product.objects.all()
-
-    return render(request, 'shop/product-list.html', context={'products': products, 'categories': categories})
-
-def product_images(request, product_id:int | None = None):
-    product = get_object_or_404(Product, id=product_id)
-    images = Images.objects.filter(product=product)
 
     context = {
         'product': product,
         'images': images,
+        'products': products,
     }
     return render(request, 'shop/product-list.html', context)
